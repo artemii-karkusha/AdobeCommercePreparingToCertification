fd49663e2cf89a903a5e9ac8395ab9f39bff3439
Artemii Karkusha <artemii.karkusha@gmail.com>
2022-11-29 20:35:24 +0100

    The custom controller with result object the Raw has been created.

---
+++ Controller/ActionsResults/Raw.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author Artemii Karkusha
+ * @copyright Copyright (c) (https://www.linkedin.com/in/artemiy-karkusha/)
+ */
+
+declare(strict_types=1);
+
+namespace ArtemiiKarkusha\AdobeCommercePreparingToCertification\Controller\ActionsResults;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\Result\RawFactory as ResultRawFactory;
+use Magento\Framework\Controller\ResultInterface;
+
+class Raw implements HttpGetActionInterface
+{
+    /**
+     * @param ResultRawFactory $resultRawFactory
+     */
+    public function __construct(private ResultRawFactory $resultRawFactory) {}
+
+    /**
+     * @inheritDoc
+     */
+    public function execute(): ResultInterface
+    {
+        return $this->resultRawFactory->create()
+            ->setContents(
+                $this->getContent()
+            );
+    }
+
+    /**
+     * @return string
+     */
+    private function getContent(): string
+    {
+        return 'A legendary actor and director reflects on the innumerable films he watched during his first decades as an audience member. ';
+    }
+}
